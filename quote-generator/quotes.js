d80574f417f76d9eb60c7e55e1c26fc14e28ea2d
Boni De Guzman <boni.deg@gmail.com>
2022-11-19 18:02:49 -0800

    Quote Generator

---
+++ quote-generator/quotes.js
@@ -0,0 +1,19 @@
+const localPeks = [
+{
+text: 'Who sows virtue reaps honour.',
+author: 'Leonardo da Vinci',
+},
+{
+text: 'Be kind whenever possible. It is always possible.',
+author: null,
+},
+{
+text: 'Talk doesnt cook rice.',
+author: "Chinese proverb",
+},
+{
+text: 'He is able who thinks he is able.',
+author: 'Buddha',
+}
+];
+
